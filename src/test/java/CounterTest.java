16cdcba12faeda17115731503a94c1881955de71
naufa <naufal.mdzulfik@gmail.com>
2026-06-05 16:58:07 +0700

    test code

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,4 +1,6 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class CounterTest {
@@ -13,7 +15,7 @@ public class CounterTest {
 		
 		testCounter.reset();
 		
-		assertEquals(0, testCounter.getCount());
+		assertEquals(1, testCounter.getCount());
 	}
 
 	@Test
